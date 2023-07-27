6c0d08e35341566bf345752aea68b56768da4f96
unknown <lincaannick14@gmail.com>
2023-07-27 15:11:09 +0300

    new

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Home () {
+ 
+
+  return (
+        <>
+        <div className="h-full w-full bg-mobile ">
+        <Navbar/>
+
+     
+            </div>
+       
+
+        </>
+  )
+}
